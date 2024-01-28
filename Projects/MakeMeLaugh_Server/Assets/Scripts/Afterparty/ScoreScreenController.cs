045a1632f7b9bf6d415bbfa923a98e15cd827b75
Richard Fine <rfine@tbrf.net>
2024-01-28 13:57:23 -0500

    Create scores screen (#30)

---
+++ Projects/MakeMeLaugh_Server/Assets/Scripts/Afterparty/ScoreScreenController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class ScoreScreenController : MonoBehaviour
+{
+    private UIDocument _document;
+    private VisualElement _container;
+    public VisualTreeAsset scoreListEntryAsset;
+
+    public void Awake()
+    {
+        _document = GetComponent<UIDocument>();
+        _container = _document.rootVisualElement.Q("ScoresContainer");
+    }
+
+    public void SetScores(IDictionary<string, int> scores)
+    {
+        var kvps = scores.OrderByDescending(kvp => kvp.Value).ToArray();
+
+        for (int i = 0; i < kvps.Length; ++i)
+        {
+            var row = scoreListEntryAsset.Instantiate();
+            row.Q<Label>(className:"player-name").text = kvps[i].Key;
+            row.Q<Label>(className:"player-score").text = kvps[i].Value.ToString();
+            row.AddToClassList($"score-{i}");
+            _container.Add(row);
+        }
+    }
+
+    void Start()
+    {
+        SetScores(new Dictionary<string, int>()
+        {
+            {"Player one", 123},
+            {"Player two", 456},
+            {"Player three", -1201}
+        });
+    }
+}
