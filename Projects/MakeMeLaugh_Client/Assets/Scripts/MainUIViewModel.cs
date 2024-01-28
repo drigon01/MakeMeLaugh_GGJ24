453209ded7a116c4c2748508230408c9c622ebc7
Richard Fine <richardf@unity3d.com>
2024-01-27 23:59:13 -0500

    Fix handling of connection settings validation / defaults

--- Projects/MakeMeLaugh_Client/Assets/Scripts/MainUIViewModel.cs
+++ Projects/MakeMeLaugh_Client/Assets/Scripts/MainUIViewModel.cs
@@ -5,6 +5,9 @@ using UnityEngine.UIElements;
 
 public class MainUIViewModel : MonoBehaviour
 {
+  private const string DefaultIPAddress = "127.0.0.1";
+  private const int DefaultPort = 7777;
+  private const string DefaultPlayerName = "JerrySeinfeld";
   private VisualElement _rootElement;
   private VisualElement _popupHost;
   [SerializeField] private VisualTreeAsset _serverSettingsTemplate;
@@ -21,6 +24,8 @@ public class MainUIViewModel : MonoBehaviour
   private VisualElement _settingsView;
   private VisualElement _waitingScreen;
   private VisualElement _jokeEditor;
+
+  private Button _connectButton;
 
   // Start is called before the first frame update
   private void Awake()
@@ -63,60 +68,72 @@ public class MainUIViewModel : MonoBehaviour
     _settingsView = new VisualElement();
     _serverSettingsTemplate.CloneTree(_settingsView);
 
-    var connectButton = _settingsView.Q<Button>("Connect");
+    _ip = PlayerPrefs.GetString("DefaultIPAddress", DefaultIPAddress);
+    _port = (ushort)PlayerPrefs.GetInt("DefaultPort", DefaultPort);
+    _name = PlayerPrefs.GetString("DefaultPlayerName", DefaultPlayerName);
+
+    _connectButton = _settingsView.Q<Button>("Connect");
     var serverIP = _settingsView.Q<TextField>("IP");
     var serverPort = _settingsView.Q<TextField>("Port");
-    var name = _settingsView.Q<TextField>("Name");
+    var nameField = _settingsView.Q<TextField>("Name");
 
-    connectButton.clicked += OnConnectButtonClicked;
+    _connectButton.clicked += OnConnectButtonClicked;
 
     serverIP.RegisterValueChangedCallback(OnIPChanged);
     serverPort.RegisterValueChangedCallback(OnPortChanged);
-    name.RegisterValueChangedCallback(OnNameChanged);
+    nameField.RegisterValueChangedCallback(OnNameChanged);
     
-    serverIP.value = "127.0.0.1";
-    serverPort.value = "7777";
-    name.value = "Joe";
+    serverIP.value = _ip;
+    serverPort.value = _port.ToString();
+    nameField.value = _name;
+  }
+
+  private void SaveToPlayerPrefs()
+  {
+    PlayerPrefs.SetString("DefaultIPAddress", _ip);
+    PlayerPrefs.SetInt("DefaultPort", _port);
+    PlayerPrefs.SetString("DefaultPlayerName", _name);
   }
 
   private void OnPortChanged(ChangeEvent<string> evt)
   {
-    if (ushort.TryParse(evt.newValue, out var result))
-    {
-      _port = result;
-    }
-    else
-    {
-      throw new ArgumentException("Incorrect value provided as port", "Port");
-    }
+    _port = ushort.TryParse(evt.newValue, out var result) ? result : (ushort)0;
+    _connectButton.SetEnabled(ValidateSettings());
   }
 
   private void OnNameChanged(ChangeEvent<string> evt)
   {
-    if (!string.IsNullOrWhiteSpace(evt.newValue))
-    {
-      _name = evt.newValue;
-    }
-    else
-    {
-      throw new ArgumentException("Missing name");
-    }
+    _name = evt.newValue;
+    _connectButton.SetEnabled(ValidateSettings());
   }
 
   private void OnIPChanged(ChangeEvent<string> evt)
   {
-    if (!string.IsNullOrWhiteSpace(evt.newValue))
-    {
-      _ip = evt.newValue;
-    }
-    else
-    {
-      throw new ArgumentException("Incorrect value provided for ip", nameof(IPAddress));
-    }
+    _ip = evt.newValue;
+    _connectButton.SetEnabled(ValidateSettings());
+  }
+
+  private bool ValidateSettings()
+  {
+    if (string.IsNullOrWhiteSpace(_ip))
+      return false;
+
+    if (_port == 0)
+      return false;
+
+    if (string.IsNullOrWhiteSpace(_name))
+      return false;
+
+    return true;
   }
 
   private void OnConnectButtonClicked()
   {
+    if (!ValidateSettings())
+      throw new InvalidOperationException("Invalid settings, can't connect");
+    
+    SaveToPlayerPrefs();
+    
     if (ConnectionManager == null)
     {
       ConnectionManager = new ConnectionManager(_ip, _port, _name);
