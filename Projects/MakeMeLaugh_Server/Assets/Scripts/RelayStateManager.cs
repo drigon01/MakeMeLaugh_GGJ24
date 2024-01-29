e315ff5157d8702cc65e927b3c4a559796d3130b
Richard Fine <rfine@tbrf.net>
2024-01-29 08:28:27 -0500

    Show a QR code on the join page (#41)
    * Add QRCoder and QRCoder.Unity libraries as package
    
    * Add custom control for displaying QR code in UIToolkit
    
    * Force QR display to always be scale-to-fit
    
    * auto-reference the QRCoder assembly
    
    * update lockfile
    
    * Add QR code to Join overlay

--- Projects/MakeMeLaugh_Server/Assets/Scripts/RelayStateManager.cs
+++ Projects/MakeMeLaugh_Server/Assets/Scripts/RelayStateManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
@@ -12,12 +13,15 @@ using UnityEngine.UIElements;
 public class RelayStateManager : MonoBehaviour
 {
     private Task _initAndAuthTask;
+    private VisualElement _overlay;
 
     public string JoinCode;
+    public string ClientHostURI = "https://richard-fine.itch.io/the-laugh-hole?secret=jal3L5588KAmK3SErcfq9IpPAw";
     
     void Start()
     {
-        GetComponent<UIDocument>().rootVisualElement.visible = false;
+        _overlay = GetComponent<UIDocument>().rootVisualElement;
+        _overlay.visible = false;
         _initAndAuthTask = InitializeAndAuthenticate();
     }
 
@@ -36,8 +40,21 @@ public class RelayStateManager : MonoBehaviour
         TransportServer.Instance.StartServerWithRelay(relayServerData);
 
         JoinCode = await RelayService.Instance.GetJoinCodeAsync(hostAllocation.AllocationId);
+        _overlay.Q<Label>("JOIN_CODE").text = JoinCode;
 
-        GetComponent<UIDocument>().rootVisualElement.Q<Label>("JOIN_CODE").text = JoinCode;
-        GetComponent<UIDocument>().rootVisualElement.visible = true;
+        var qrCode = _overlay.Q<QRCoder.Unity.QRCodeDisplay>("QR_CODE");
+        if (!string.IsNullOrWhiteSpace(ClientHostURI))
+        {
+            var uri = new UriBuilder(ClientHostURI);
+            uri.Query = string.Join("&", uri.Query, "joinCode=" + JoinCode);
+            qrCode.value = uri.ToString();
+            qrCode.visible = true;
+        }
+        else
+        {
+            qrCode.visible = false;
+        }
+
+        _overlay.visible = true;
     }
 }
